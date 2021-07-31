c2a837e590eafe1e9917e7e36e680ad7de111e95
Alessandra <68104089+AlessandraMonaco@users.noreply.github.com>
2021-07-31 18:39:45 +0200

    Added title and clear button

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import 'normalize.css'
-import './index.scss'
+import './style.scss'
+//var sass = require('node-sass'); // or require('sass');
+//var result = sass.renderSync({file: "./style.scss"});
+//console.log(result);
+
+
+// This script is used to run webcode in node.js
+// It creates a local server reading html file on port 9999
+
+/*var http = require('http');
+var fs = require('fs'); //file system module
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(9999);*/
