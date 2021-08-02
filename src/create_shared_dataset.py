ce4bc1422460ccafe8117275a7131ee4a352d158
Alessandra <68104089+AlessandraMonaco@users.noreply.github.com>
2021-08-02 13:04:00 +0200

    Created full_data.csv with created_shared_dataset.py

---
+++ src/create_shared_dataset.py
@@ -0,0 +1,31 @@
+# This script creates a single common dataset that should be shared by all
+# the visualizations, merging the existing data sources into one csv
+# VsCode note: use interpreter 3.7
+
+# Import libraries
+import pandas as pd
+import numpy as np
+
+# Read data from csv
+path = "C:/Users/user/Documents/GitHub/Visual-Analytics/dataset/"
+ctg = pd.read_csv(path+"prod_cat_info.csv") #categories csv
+cst = pd.read_csv(path+"Customer.csv")
+tr = pd.read_csv(path+"Transactions.csv")
+
+# Handle mistyped negative values (replace with positive values)
+tr['Qty'] = np.abs(tr['Qty'])
+tr['total_amt'] = np.abs(tr['total_amt'])
+tr.head()
+
+# Merge data
+#Merge transaction data with category information
+tr_merged = pd.merge(tr, ctg,  how='left', left_on=['prod_cat_code','prod_subcat_code'], 
+right_on = ['prod_cat_code','prod_sub_cat_code'])
+#Merge with customer information
+tr_final = pd.merge(tr_merged, cst,  how='left', left_on=['cust_id'], 
+right_on = ['customer_Id'])
+tr_final = tr_final.sort_values(by=["tran_date"])
+tr_final.head()
+
+# Write final dataset to csv
+tr_final.to_csv(path+'full_data.csv', index=False)
--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import 'normalize.css'
-import './style.scss'
+//import 'normalize.css'
+//import './style.scss'
 //var sass = require('node-sass'); // or require('sass');
 //var result = sass.renderSync({file: "./style.scss"});
 //console.log(result);
+//import * as d3 from "d3";
+var d3 = require("d3");
+
+const csv = require('d3-fetch').csv;
 
 
-// This script is used to run webcode in node.js
-// It creates a local server reading html file on port 9999
 
-/*var http = require('http');
-var fs = require('fs'); //file system module
-http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(9999);*/
+csv("C:/Users/user/Documents/GitHub/Visual-Analytics/dataset/full_data.csv", function(csv_data){
+                    
+  var nested_data = d3.nest()
+      .key(function(d) { return d.prod_cat; })
+      .key(function(d) { return d.prod_subcat; })
+      .entries(csv_data);
+      
+  console.debug(nested_data);
+  alert(JSON.stringify(nested_data));
+
+});
